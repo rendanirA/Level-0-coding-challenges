b03ea0696708bb96e18a379fb5cc3685c8ccb2d5
Rendani <rratsethana@gmail.com>
2022-04-12 11:30:19 +0200

    task_0.6.py file edited commit

---
+++ task_0.6.py
@@ -0,0 +1,12 @@
+
+def max_number(x ,y, z): 
+
+    if x > y and x > z :
+        print(x ," is a maximum value")
+    elif y>x and y > z: 
+      print( y," is a maximum value")
+    elif z > x and z > y :
+        print (z ," is a maximum value")
+      
+print (max_number(9,81,10))
+     
