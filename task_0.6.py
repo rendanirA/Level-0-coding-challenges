21363351c2f796dfad4a854c15ad14515906fc1b
Rendani <rratsethana@gmail.com>
2022-04-14 15:03:05 +0200

    task_0.6 corrected commit

--- task_0.6.py
+++ task_0.6.py
@@ -1,12 +1,11 @@
+def max_number(number1, number2, number3):
 
-def max_number(x ,y, z): 
+    if number1 > number2 and number1 > number3:
+        print(number1, " is a maximum value")
+    elif number2 > number1 and number2 > number3:
+        print(number2, " is a maximum value")
+    elif number3 > number1 and number3 > number2:
+        print(number3, " is a maximum value")
 
-    if x > y and x > z :
-        print(x ," is a maximum value")
-    elif y>x and y > z: 
-      print( y," is a maximum value")
-    elif z > x and z > y :
-        print (z ," is a maximum value")
-      
-print (max_number(9,81,10))
-     
+
+print(max_number(9, 81, 10))
