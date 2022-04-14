541835f325ad0439e4050e596c12ffaa142e72db
Rendani <rratsethana@gmail.com>
2022-04-14 14:59:10 +0200

    task_0.5 corrected commit

--- task_0.5.py
+++ task_0.5.py
@@ -1,4 +1,20 @@
+from math import sqrt
+def area_of_a_triangle(side1, side2, side3):
+    semi_parimeter = 1 / 2 * (side1 + side2 + side3)
+    semi_perimeter_minus_side1 = semi_parimeter - side1
+    semi_perimeter_minus_side2 = semi_parimeter - side2
+    semi_perimeter_minus_side3 = semi_parimeter - side3
+    area = (
+        semi_parimeter
+        * (semi_perimeter_minus_side1)
+        * (semi_perimeter_minus_side2)
+        * (semi_perimeter_minus_side3)
+    )
+    return sqrt(area)
 
-def area_of_a_triangle(x,y,z):
-    return (1/2*(x + y + z))
-print(area_of_a_triangle(5,6,7))
+
+print(area_of_a_triangle(3, 4, 5))
+
+
+
+
