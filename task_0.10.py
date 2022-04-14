3e61b5a8e0512eb83dc9f0565b8aa12252f53498
Rendani <rratsethana@gmail.com>
2022-04-14 14:56:05 +0200

    task_0.10 corrected

--- task_0.10.py
+++ task_0.10.py
@@ -1,14 +1,15 @@
 def find_common_characters(string1, string2):
-    res = " "
+    res = ""
     for char in string1:
         if char in string2:
             res = res + char
     else:
-        x = " ".join(sorted(set(res), key=res.index))
+        x = ",".join(sorted(set(res), key=res.index))
     return x
 
 
-string1 = "house"
-string2 = "computers"
-common_characters = find_common_characters(string1, string2)
-print("common letters: ", common_characters, end=",")
+string1 = "House"
+string2 = "Computers"
+common_characters = find_common_characters(string1.lower(), string2.lower())
+print(common_characters, sep=",")
+
