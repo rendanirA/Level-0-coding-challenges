41fdd35dfca3ad45fc1071c12e915582d1c027d5
Rendani <rratsethana@gmail.com>
2022-04-14 15:11:54 +0200

    task_0.4 corrected commit

--- task_0.4.py
+++ task_0.4.py
@@ -1,7 +1,9 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        print("Even")
+    elif number % 2 == 1:
+        print("Odd")
 
-def even_or_odd (x):
-     if x % 2==0 :
-        print ("Even") 
-     elif x % 2 ==1 :
-        print ("Odd")
+
 even_or_odd(2)
+
