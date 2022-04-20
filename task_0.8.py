b8c525598069da2bb7adbfea1a69618c4c3ea978
Rendani <rratsethana@gmail.com>
2022-04-20 21:56:39 +0200

    task_0.8.py edited commit

--- task_0.8.py
+++ task_0.8.py
@@ -2,15 +2,20 @@ def convert_any_number_to_time(num):
     hour = num // 60
     minutes = num % 60
 
-    if num // 60 == 1 or num % 60 == 1:
-        print(hour, "Hour ,", minutes, "minutes")
-    elif num // 60 == 1 and num % 60 >= 1:
+    if num // 60 == 1 and num % 60 == 0:
+        print(hour, "Hour ,", minutes, "minute")
+    elif num // 60 == 1 and num % 60 == 1:
         print(hour, "hour ,", minutes, "minute")
+    elif num // 60 == 1 and num % 60 > 1:
+        print(hour, "Hour ,", minutes, "minutes")
+    elif num // 60 > 1 and num % 60 == 0:
+        print(hour, "Hours ,", minutes, "minute")
     elif num // 60 > 1 and num % 60 == 1:
         print(hour, "Hours ,", minutes, "minute")
+    elif num // 60 > 1 and num % 60 > 1:
+        print(hour, "Hours ,", minutes, "minutes")
     else:
         print(hour, "Hours ,", minutes, "minutes")
 
 
-convert_any_number_to_time(60)
-
+convert_any_number_to_time(242)
