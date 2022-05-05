e45c01425eebfd5c8a7cbc814c923056d93d8ff5
Rendani <rratsethana@gmail.com>
2022-05-06 00:14:13 +0200

    task_0.5 changed

--- task_0.5.py
+++ task_0.5.py
@@ -9,7 +9,7 @@ def area_of_a_triangle(side1, side2, side3):
         * (semi_perimeter_minus_side2)
         * (semi_perimeter_minus_side3)
     )
-    return sqrt(area)**0.5
+    return (area) ** 0.5
 
 
 print(area_of_a_triangle(3, 4, 5))
