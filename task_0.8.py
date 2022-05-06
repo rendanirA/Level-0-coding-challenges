dfdd2511d2a133d6bf297188af78346388dcd763
Rendani <rratsethana@gmail.com>
2022-05-06 13:18:36 +0200

    all files renamed

--- task_0.8.py
+++
@@ -1,9 +0,0 @@
-def convert_any_number_to_time(num):
-    hour = num // 60
-    minutes = num % 60
-    print(
-        f"{hour} hour{'s' if hour>=1 else ''} , {minutes} minute{'s' if minutes>=1 else ''}  "
-    )
-
-
-convert_any_number_to_time(312)
